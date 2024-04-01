af2339c5f4a37185ee947814d950152e90228aaa
Aziz <ngwk.zz@outlook.com>
2024-04-01 17:32:03 +0300

    Hypermedia pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,21 +1,20 @@
-#!/usr/bin/evn python3
+#!/usr/bin/env python3
 """
 Implements a get_hyper method that takes the same arguments
 (and defaults) as get_page
 """
 import csv
 import math
-from typing import List, Tuple, Dict, Any
+from typing import Dict, List, Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     Return a tuple of size two containing a start index and an end index
     """
-    start_index = (page - 1) * page_size
-    end_index = start_index + page_size
-
-    return (start_index, end_index)
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
 
 
 class Server:
@@ -24,6 +23,8 @@ class Server:
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
+        """Initializes a new Server instance.
+        """
         self.__dataset = None
 
     def dataset(self) -> List[List]:
@@ -42,35 +43,27 @@ class Server:
         Return the appropriate page of the dataset based
         on pagination params
         """
-        assert isinstance(
-                page, int
-                ) and page > 0, "Page must be an int greater than 0"
-        assert isinstance(
-                page_size, int
-                ) and page > 0, "Page size must be an int greater than 0"
-
-        start_index, end_index = index_range(page, page_size)
-        dataset = self.dataset()
-
-        if start_index >= len(dataset):
+        assert type(page) == int and type(page_size) == int
+        assert page > 0 and page_size > 0
+        start, end = index_range(page, page_size)
+        data = self.dataset()
+        if start > len(data):
             return []
+        return data[start:end]
 
-        return dataset[start_index:end_index]
-
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Any]:
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
         """
         Return a dict containing hypermedia metadata
         """
-        dataset_page = self.get_page(page, page_size)
-        total_pages = math.ceil(len(self.dataset()) / page_size)
-        next_page = page + 1 if page < total_pages else None
-        prev_page = page - 1 if page > 1 else None
+        page_data = self.get_page(page, page_size)
+        start, end = index_range(page, page_size)
+        total_pages = math.ceil(len(self.__dataset) / page_size)
 
         return {
-            "page_size": len(dataset_page),
-            "page": page,
-            "data": dataset_page,
-            "next_page": next_page,
-            "prev_page": prev_page,
-            "total_pages": total_pages
+            'page_size': len(page_data),
+            'page': page,
+            'data': page_data,
+            'next_page': page + 1 if end < len(self.__dataset) else None,
+            'prev_page': page - 1 if start > 0 else None,
+            'total_pages': total_pages,
         }
