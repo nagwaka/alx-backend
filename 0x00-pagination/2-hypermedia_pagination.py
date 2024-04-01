17d8abf47e5ab9c1a819f88fabf30d9e11e58e8c
Aziz <ngwk.zz@outlook.com>
2024-04-01 17:37:58 +0300

    Hypermedia pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -12,9 +12,10 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     Return a tuple of size two containing a start index and an end index
     """
-    start = (page - 1) * page_size
-    end = start + page_size
-    return (start, end)
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+
+    return (start_index, end_index)
 
 
 class Server:
