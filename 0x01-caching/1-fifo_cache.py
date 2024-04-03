47ee5c6d4a2319af351490f8fc3912c5d82f4307
Aziz <ngwk.zz@outlook.com>
2024-04-03 09:12:37 +0300

    FIFO caching

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,33 @@
+#!/usr/bin/evn python3
+"""Class FIFOCache that inherits from BaseCaching
+   and is a caching system
+"""
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class FIFOCache(BaseCaching):
+    """Inherits from BaseCaching and implements FIFO caching algorithm
+    """
+    def __init__(self):
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is None or item is None:
+            return
+
+        self.cache_data[key] = item
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            discard_first_key, _ = self.cache_data.popitem(False)
+            print(f'DISCARD: { discard_first_key}')
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key is None or Key not in self.cache_data:
+            return None
+        return self.cache_data[key]
