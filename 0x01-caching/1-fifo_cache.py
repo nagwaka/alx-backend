71f1688dfaa327a796302a2f63f40b4ad591ee0b
Aziz <ngwk.zz@outlook.com>
2024-04-03 09:15:51 +0300

    FIFO caching

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -23,11 +23,9 @@ class FIFOCache(BaseCaching):
 
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
             discard_first_key, _ = self.cache_data.popitem(False)
-            print(f'DISCARD: { discard_first_key}')
+            print(f'DISCARD: {discard_first_key}')
 
     def get(self, key):
         """ Get an item by key
         """
-        if key is None or Key not in self.cache_data:
-            return None
-        return self.cache_data[key]
+        return self.cache_data.get(key, None)
