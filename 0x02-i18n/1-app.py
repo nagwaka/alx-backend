5d03062ad121bf7d57fda0c3337edf5064ba6ebf
Aziz <ngwk.zz@outlook.com>
2024-04-09 18:24:05 +0300

    Basic Babel setup

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -13,12 +13,13 @@ class Config:
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
-    
-    
+
+
 app = Flask(__name__)
 app.config.from_object(Config)
 
 babel = Babel(app)
+
 
 @app.route('/')
 def get_index() -> str:
