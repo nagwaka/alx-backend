8c5839a1bdb2de2c5f838a4ee3f5c9bf3d8be873
Aziz <ngwk.zz@outlook.com>
2024-04-09 17:57:34 +0300

    Basic Babel setup

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,13 +7,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/", methods=["GET"])
+@app.route('/')
 def index():
     """
     Outputs a message
     """
-    return render_template("0-index.html")
+    return render_template('0-index.html')
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -2,7 +2,7 @@
 """
 Basic Babel setup
 """
-from flask import Flask
+from flask import Flask, render_template
 from flask_babel import Babel
 
 
@@ -19,3 +19,14 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 babel = Babel(app)
+
+@app.route('/')
+def get_index() -> str:
+    """
+    Outputs a message
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
