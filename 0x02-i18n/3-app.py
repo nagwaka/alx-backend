64c613a9d2f1da27635ff9b1dabc81cf24d77a57
Aziz <ngwk.zz@outlook.com>
2024-04-09 20:50:12 +0300

    Parametrize templates

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Get locale from request
+Parametrize templates
 """
 from flask import Flask, render_template, request
 from flask_babel import Babel
@@ -34,7 +34,7 @@ def index() -> str:
     """
     Home page
     """
-    return render_template('2-index.html')
+    return render_template('3-index.html')
 
 
 if __name__ == "__main__":
